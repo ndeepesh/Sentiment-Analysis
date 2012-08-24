bb5682f50ef0c15c2dbf1bd18993345634065e0c
Dheeraj Rajagopal <dheeraj.gopal@gmail.com>
2012-08-24 18:40:31 +0530

    Evaluation set ready

--- empathy/prob_eval.py
+++ empathy/prob_eval.py
@@ -3,22 +3,22 @@
 import MicrosoftNgram
 
 s = MicrosoftNgram.LookupService(model='urn:ngram:bing-body:apr10:5')
- 
-f = open('emotion.tuples', 'r')
+
+f = open('random.tuples','r')
 tuples = f.readlines()
 f.close()
 
 for i in range(len(tuples)):
 	tuples[i] = tuples[i].replace("\n","")
-	tuples[i] = tuples[i].replace("  "," ")
 
-f = open('EmotionTuples.Prob', 'w')
+f = open('EvalTuples.Prob','w')
+
 
 for i in tuples:
 	try:
 		prob = s.GetConditionalProbability(i)
-		print >> f, i , "\t", prob 
+		print >> f, i , "\t", prob
 		f.flush()
-	except:
+	except: 
 		continue
 f.close()
